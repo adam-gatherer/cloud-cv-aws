1a7fac7c21dc6b8f736edbc3578c5f22b5d97359
Adam Gatherer <adam@gatherer.tech>
2023-07-30 19:42:06 +0000

    Altered index.html/js

--- site/index.js
+++ site/index.js
@@ -4,7 +4,7 @@ const counter = document.querySelector(".counter-number"); //select the element 
 async function updateCounter() { //function that does a fetch request
 	let response = await fetch ("https://bujk7hizyolll7qscxgtjcodpy0qzhzx.lambda-url.eu-west-1.on.aws/");
 	let data = await response.json(); //stores response as variable called "data"
-	counter.innerHTML = `Views: ${data}`; //updates counter in HTML file to use data variable
+	counter.innerHTML = `Page views: ${data}`; //updates counter in HTML file to use data variable
 }
 
 updateCounter();
