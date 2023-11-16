21fb085bc6b0dd60be7aa29d01e8e4ff8266a282
Adam <adam@gatherer.tech>
2023-11-16 15:09:17 +0000

    Formatted iam-role-policy.tf

--- infrastructure/iam-role-policy.tf
+++ infrastructure/iam-role-policy.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "iam_role_for_cloud_cv_tf" {
-  name = "iam_role_for_cloud_cv_tf"
-  assume_role_policy =  <<EOF
+  name               = "iam_role_for_cloud_cv_tf"
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -18,36 +18,36 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_cloud_cv_tf" {
-  name = "aws_iam_policy_for_cloud_cv_tf"
-  path = "/"
+  name        = "aws_iam_policy_for_cloud_cv_tf"
+  path        = "/"
   description = "AWS IAM policy for managing the cloud cv project role"
   policy = jsonencode(
     {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Action": [
-                    "logs:CreateLogGroup",
-                    "logs:CreateLogStream",
-                    "logs:PutLogEvents"
-                ],
-                "Resource": "arn:aws:logs:*:*:*",
-                "Effect": "Allow"
-            },
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "dynamodb:UpdateItem",
-                    "dynamodb:GetItem"
-                ],
-                "Resrouce": "arn:aws:dynamodb:*:*:table/cloud-cv-tf"
-            }
-        ]
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource" : "arn:aws:logs:*:*:*",
+          "Effect" : "Allow"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "dynamodb:UpdateItem",
+            "dynamodb:GetItem"
+          ],
+          "Resrouce" : "arn:aws:dynamodb:*:*:table/cloud-cv-tf"
+        }
+      ]
     }
   )
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role = aws_iam_role.iam_role_for_cloud_cv_tf.name
+  role       = aws_iam_role.iam_role_for_cloud_cv_tf.name
   policy_arn = aws_iam_policy.iam_policy_for_cloud_cv_tf.arn
 }
