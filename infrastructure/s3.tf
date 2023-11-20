0ca0cebd792b82e3517e7dc33bfaeac7baf9a8df
Adam <adam@gatherer.tech>
2023-11-20 15:02:13 +0000

    Added comments

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -7,13 +7,16 @@ resource "random_string" "random" {
 }
 
 resource "aws_s3_bucket" "cloud-cv-tf" {
+  # Uses random string to ensure globally unique bucket name
   bucket = "${random_string.random.result}-${var.cv_bucket_name}"
   tags = var.global_tags
 }
 
 data "aws_iam_policy_document" "cloud-cv-tf" {
+  # Policy to permit GetObject actions on the bucket
   statement {
     actions = ["s3:GetObject"]
+    # Identifies the bucket by its property
     resources = ["${aws_s3_bucket.cloud-cv-tf.arn}/*"]
 
     principals {
@@ -24,12 +27,14 @@ data "aws_iam_policy_document" "cloud-cv-tf" {
 }
 
 resource "aws_s3_bucket_policy" "cloud-cv-tf" {
+  # Applies the policy to the bucket
   bucket = aws_s3_bucket.cloud-cv-tf.id
   policy = data.aws_iam_policy_document.cloud-cv-tf
 }
 
 
 resource "aws_s3_bucket_website_configuration" "cloud-cv-tf" {
+  
   bucket = aws_s3_bucket.cloud-cv-tf.bucket
 
   index_document {
