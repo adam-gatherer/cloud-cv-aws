b587af19710df314f1ec9ea58be27e8452690a3d
Adam <adam@gatherer.tech>
2023-11-20 14:04:32 +0000

    Updated s3.tf

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -8,16 +8,31 @@ resource "random_string" "random" {
 
 resource "aws_s3_bucket" "cloud-cv-tf" {
   bucket = "${random_string.random.result}-${var.cv_bucket_name}"
-  tags ={
-    Name = "s3-cloud-cv-tf"
+  tags = var.global_tags
+}
+
+data "aws_iam_policy_document" "cloud-cv-tf" {
+  statement {
+    actions = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.cloud-cv-tf.arn}/*"]
+
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
   }
 }
 
+
 resource "aws_s3_bucket_website_configuration" "cloud-cv-tf" {
-  bucket = aws_s3_bucket.cloud-cv-tf.id
+  bucket = aws_s3_bucket.cloud-cv-tf.bucket
 
   index_document {
     suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
   }
 
 
