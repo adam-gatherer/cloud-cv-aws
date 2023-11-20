d4d0eee8d3dda186d66a43c4b6c0e3a07480729d
Adam <adam@gatherer.tech>
2023-11-20 15:25:15 +0000

    Rebuilt cdn.tf

--- infrastructure/cdn.tf
+++ infrastructure/cdn.tf
@@ -1,48 +1,58 @@
 resource "aws_cloudfront_distribution" "cloud-cv-tf" {
-    origin {
-        domain_name = aws_s3_bucket_website_configuration.aws_cloudfront_distribution.
-        origin_id   = "OriginaName"
-        custom_origin_config {
-          
-          origin_ssl_protocols          = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-          http_port                     = 80
-          https_port                    = 443
-          origin_keepalive_timeout      = 5
-          origin_protocol_policy        = "http-only"
-        }
-    }
-    viewer_certificate {
-      
-      acm_certificate_arn   = aws_acm_certificate.cloud-cv-tf.acm_certificate_arn
-      ssl_support_method    = "sni-only"
-    }
-    enabled         = true
-    is_ipv6_enabled = true
-    restrictions {
-      
-      geo_restriction {
-        restriction_type = "none"
-      }
-    }
-    aliases = ["tf.cv.gatherer.tech", "www.tf.cv.gatherer.tech"]
-    default_cache_behavior {
-      target_origin_id          = "OriginName"
-      viewer_protocol_policy    = "redirect-to-https"
-      allowed_methods           = ["GET", "HEAD", "OPTIONS"]
-      cached_methods            = ["GET", "HEAD", "OPTIONS"]
-      min_ttl                   = 0
-      default_ttl               = 0
-      max_ttl                   = 0
-      forwarded_values {
-        query_string    = false
-        headers         = ["*"]
-        cookies {
-          forward = "none"
-        }
-      }
-    }
-}
+  origin {
+    domain_name = aws_s3_bucket.cloud-cv-tf.website_endpoint
+    origin_id = "S3-${var.cv_bucket_name}"
 
-resource "aws_cloudfront_origin_access_identity" "cloud-cv-tf" {
-  comment = "OAI for cv website"
+    custom_origin_config {
+      http_port = 80
+      https_port = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
+
+  enabled = true
+  is_ipv6_enabled = true
+  default_root_object = "index.html"
+
+  aliases = ["www.tf.cv.gatherer.tech", "tf.cv.gatherer.tech"]
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "S3-${var.cv_bucket_name}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      # Come back to this later to update
+      locations = ["US", "CA", "GB", "DE", "FR"]
+    }
+  }
+
+  tags = var.global_tags
+
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn = aws_acm_certificate.cloud-cv-tf.arn
+    minimum_protocol_version = "TLSv1"
+    ssl_support_method = "sni-only"
+  }
+
+
 }
